d36d8fb82d9067f7b21b90c992c592ab3e8893b6
Raphael Pereira <75498332+RaphaelPereira88@users.noreply.github.com>
2020-12-09 09:03:31 +0000

    Update weather.py

--- weather.py
+++ weather.py
@@ -5,17 +5,17 @@ API_LOCATION = '/api/location/search/?query='
 API_WEATHER = '/api/location/'  # + woeid
 
 def fetch_location(query):
-    return requests.get(API_ROOT + API_LOCATION + query).json()
+    return requests.get(API_ROOT + API_LOCATION + query).json()           # convert data from json text to python dictionary accordint to user input.
 
 def fetch_weather(woeid):
-    return requests.get(API_ROOT + API_WEATHER + str(woeid)).json()
+    return requests.get(API_ROOT + API_WEATHER + str(woeid)).json()       # ask city name to get WOEID and use it to get weather data
 
-def disambiguate_locations(locations):
+def disambiguate_locations(locations):                                    # help find city if city name not  fully typed
     print("Ambiguous location! Did you mean:")
     for loc in locations:
         print(f"\t* {loc['title']}")
 
-def display_weather(weather):
+def display_weather(weather):                                                      # function that displays the weather for the 6 coming days, loop
     print(f"Weather for {weather['title']}:")
     for entry in weather['consolidated_weather']:
         date = entry['applicable_date']
@@ -25,19 +25,19 @@ def display_weather(weather):
         print(f"{date}\t{state}\thigh {high:2.1f}°C\tlow {low:2.1f}°C")
 
 def weather_dialog():
-    try:
+    try:                                                                               # try     except  to avoid system crash, a tell the user  the user the reason. 
         where = ''
         while not where:
-            where = input("Where in the world are you? ")
-        locations = fetch_location(where)
+            where = input("Where in the world are you? ")                              # ask user what city they are in . input
+        locations = fetch_location(where)                                              # help find city if city name not  fully typed
         if len(locations) == 0:
             print("I don't know where that is.")
         elif len(locations) > 1:
             disambiguate_locations(locations)
         else:
-            woeid = locations[0]['woeid']
-            display_weather(fetch_weather(woeid))
-    except requests.exceptions.ConnectionError:
+            woeid = locations[0]['woeid']                                                #use Woeid to get weather data
+            display_weather(fetch_weather(woeid))                                       # display the weather
+    except requests.exceptions.ConnectionError:                                          # to avoir system crash si server not responding per example.   here we use try ...  except 
         print("Couldn't connect to server! Is the network up?")
 
 if __name__ == '__main__':
